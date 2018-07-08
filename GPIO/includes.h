bbec538c9a68da7987846b02598ec3e11a22f96f
Виталий Кушла <KushlaVR@gmail.com>
2018-07-08 11:08:58 +0300

    1

---
+++ GPIO/includes.h
@@ -0,0 +1,8 @@
+#pragma once
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+#include <iostream>
+#include <WinSock2.h>
+#include <winsock.h>
+#include "SClient.h"
+using namespace std;
