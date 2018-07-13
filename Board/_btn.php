af87ad1398d66cf897d021aebfa41a610a01a96b
KushlaVR <kushlavr@gmail.com>
2018-07-13 18:04:50 +0300

    auto commit

--- Board/_btn.php
+++ Board/_btn.php
@@ -1,17 +1,16 @@
 <div class="row">
 <div class="col-sm-3">
-
 <div class="input-group">
-<div class="form-control" id="p<?php echo $model->id ?>">
-    <label>Port<?php echo $model->id ?></label>
-    <label class="switch float-right">
-      <input type="checkbox" 
-	      class="checkbox cb-p<?php echo $model->id ?>"
-		  data-id = "<?php echo $model->id ?>" 
-		  data-cmd = "<?php echo $model->cmd ?>"/>
-	  <span class="slider"></span>
-    </label>
-</div>
+	<div class="form-control" id="p<?php echo $model->id ?>">
+		<label>Port<?php echo $model->id ?></label>
+		<label class="switch float-right">
+			<input type="checkbox" 
+				class="checkbox cb-p<?php echo $model->id ?>"
+				data-id = "<?php echo $model->id ?>" 
+				data-cmd = "<?php echo $model->cmd ?>"/>
+			<span class="slider"></span>
+		</label>
+	</div>
 </div>
 
 </div>
