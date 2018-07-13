88d948e7dfd1597e52c6ca3422da9946e042fd79
Віталій Кушла <KushlaVR@gmail.com>
2018-07-13 11:10:29 +0300

    Update index.php

--- Board/index.php
+++ Board/index.php
@@ -1,29 +1,3 @@
-<?php 
-    function __autoload($class_name) {
-        require_once $class_name . '.php';
-    }
-    __autoload('Board');
-    
-
-    try {
-        $values = urldecode($_GET["cmd"]);
-        if ($values!=NULL){
-            $board = new Board;
-            if ($board->isCommand($values)){
-                //var_dump($board);
-                $board->gpio_exec();
-            }
-        }
-    }
-    catch (Exception $e) {
-        echo 'Виникла помилка: ',  $e->getMessage(), "\n";
-    }
-
-    $ui = urldecode($_GET["ui"]);
-    if ($ui=="false"){
-        return;
-    }
-?>
 <!DOCTYPE html>
 
 
