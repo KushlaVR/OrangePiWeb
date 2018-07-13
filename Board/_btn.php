2ec1d1721c015d9e8bc70dd33b1a9edcbc4652e2
Віталій Кушла <KushlaVR@gmail.com>
2018-07-13 10:50:42 +0300

    Update _btn.php

--- Board/_btn.php
+++ Board/_btn.php
@@ -1,5 +1,5 @@
 
 <div class="btn-group" role="group">
-  <button type="button" class="btn btn-primary btn-on" data-id = "<?php echo $model ?>" data-state="1">On</button>
-  <button type="button" class="btn btn-secondary btn-off" data-id = "<?php echo $model ?>" data-state="0">Off</button>
+  <button type="button" class="btn btn-success btn-on" data-id = "<?php echo $model ?>" data-state="1">On</button>
+  <button type="button" class="btn btn-light btn-off" data-id = "<?php echo $model ?>" data-state="0">Off</button>
 </div>
