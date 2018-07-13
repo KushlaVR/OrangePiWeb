fc5af581922652602c402fc773f08b351e64436a
Віталій Кушла <KushlaVR@gmail.com>
2018-07-13 10:29:56 +0300

    Create _btn.php

---
+++ Board/_btn.php
@@ -0,0 +1,5 @@
+
+<div class="btn-group" role="group">
+  <button type="button" class="btn btn-primary btn-on" data-id = "<?php echo $model ?>" >On</button>
+  <button type="button" class="btn btn-secondary btn-off" data-id = "<?php echo $model ?>">Off</button>
+</div>
