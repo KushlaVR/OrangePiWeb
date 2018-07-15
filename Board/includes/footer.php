8631f52c191046a83b3042e804002955c671419c
Виталий Кушла <KushlaVR@gmail.com>
2018-07-15 12:39:08 +0300

    викинув дубль зображення

--- Board/includes/footer.php
+++ Board/includes/footer.php
@@ -12,15 +12,11 @@
 <h3>Як виглядає плата?</h3>
 <br />
 <div class="row">
-    <div class="col-sm-4">
+    <div class="col-md-6">
         <img src="./img/board-1.jpg" alt="Image" width="300" />
         <p>Опис фкнкіцй</p>
     </div>
-    <div class="col-sm-4">
-        <img src="./img/board-2.jpg" alt="Image" width="300" />
-        <p>Вигляд під кутом</p>
-    </div>
-    <div class="col-sm-4">
+    <div class="col-md-6">
         <img src="./img/board-3.jpg" alt="Image" width="300" />
         <p>Вигляд з боку</p>
     </div>
